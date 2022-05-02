6566d0d21780b246033898b75b1af78792ab92c6
akhileshkr236 <akhileshkr236@gmail.com>
2022-05-02 23:51:19 +0530

    dark theme added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject, OnInit, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'insta-portfolio-final';
+  theme: Theme = 'light-theme';
+
+
+  constructor(
+    @Inject(DOCUMENT) private document: Document,
+    private renderer: Renderer2
+  ) {
+
+  }
 
   onActivate(event) {
     let scrollToTop = window.setInterval(() => {
@@ -17,4 +27,19 @@ export class AppComponent {
       }
     }, 16);
   }
+
+  ngOnInit(): void {
+    this.initializeTheme();
+   }
+  
+   switchTheme(){
+     this.document.body.classList.replace(
+       this.theme,
+       this.theme === 'light-theme' ? (this.theme = 'dark-theme') : (this.theme = 'light-theme')
+     );
+   }
+
+  initializeTheme = (): void => this.renderer.addClass(this.document.body, this.theme);
 }
+
+export type Theme = 'light-theme' | 'dark-theme';
